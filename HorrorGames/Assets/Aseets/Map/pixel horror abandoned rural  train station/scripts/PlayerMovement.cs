7ef3c5bb82bbcc993b526f63ca4b19b64eeb5cf2
Mokou2201207 <24051905@d.anabuki-college.net>
2026-04-19 17:29:26 +0900

    電車を追加

--- HorrorGames/Assets/Aseets/Map/pixel horror abandoned rural  train station/scripts/PlayerMovement.cs
+++ HorrorGames/Assets/Aseets/Map/pixel horror abandoned rural  train station/scripts/PlayerMovement.cs
@@ -1,43 +1,46 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-namespace littleDog
+public class PlayerMovement : MonoBehaviour
 {
-    [RequireComponent(typeof(CharacterController))]
-    public class PlayerMovement : MonoBehaviour
+    [Header("プレイヤー"), SerializeField]
+    private CharacterController controller;
+    [Header("移動速度"), SerializeField]
+    private float speed = 12f;
+    [Header("重力"), SerializeField]
+    private float gravity = -40f;
+    [Header("ジャンプ力"), SerializeField]
+    private float JumpHight = 1;
+    Vector3 V;
+    private void Awake()
     {
+        //格納
+        controller = gameObject.GetComponent<CharacterController>();
+    }
 
-        private CharacterController controller;
-        public float speed = 12f;
-        public float gravity = -9.81f;
-        public float JumpHight = 3;
-         Vector3 V;
-        private void Awake()
+
+    void Update()
+    {
+        if (controller.isGrounded && V.y < 0)
         {
-            controller = gameObject.GetComponent<CharacterController>();
+            V.y = -2f;
         }
-
-        // Update is called once per frame
-        void Update()
+        //入力
+        float X = Input.GetAxis("Horizontal");
+        float Z = Input.GetAxis("Vertical");
+        //移動
+        Vector3 M = transform.right * X + transform.forward * Z;
+        controller.Move(M * speed * Time.deltaTime);
+        //ジャンプ
+        if (Input.GetButtonDown("Jump"))
         {
-             if (MouseLook.CanMove == false) return;
-            if (controller.isGrounded && V.y < 0)
-            {
-                V.y = -2f;
-            }
-            float X = Input.GetAxis("Horizontal");
-            float Z = Input.GetAxis("Vertical");
-            Vector3 M = transform.right * X + transform.forward * Z;
-            controller.Move(M * speed * Time.deltaTime);
-            if (Input.GetButtonDown("Jump"))
-            {
-                V.y = Mathf.Sqrt(JumpHight * -2f * gravity);
-            }
-            V.y += gravity * Time.deltaTime;
-            controller.Move(V * Time.deltaTime);
-
-
+            V.y = Mathf.Sqrt(JumpHight * -2f * gravity);
         }
+        V.y += gravity * Time.deltaTime;
+        controller.Move(V * Time.deltaTime);
+
 
     }
+
 }
+
