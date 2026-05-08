899beb33d905bcf41e520658b1af9fbb91121ae7
kimuraasashin <24051902@d.anabuki-college.net>
2026-05-08 15:11:39 +0900

    アニメーション追加

--- HorrorGames/Assets/Sin/Scene/EnemyAI.cs
+++ HorrorGames/Assets/Sin/Scene/EnemyAI.cs
@@ -6,28 +6,32 @@ public class EnemyAI : MonoBehaviour
     public Transform target;
 
     private NavMeshAgent agent;
+    private Animator anim;
 
     void Start()
     {
         agent = GetComponent<NavMeshAgent>();
+        anim = GetComponent<Animator>();
     }
 
     void Update()
     {
-        transform.LookAt(target);
-
-        if (target != null)
-        {
-            // ターゲットの位置を目的地として設定
-            agent.destination = target.position;
-        }
+        
     }
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.name == "Player")
         {
-            Debug.Log("プレイヤー発見！");
+            anim.SetBool("Change", true);
+
+            transform.LookAt(target);
+
+            if (target != null)
+            {
+                // ターゲットの位置を目的地として設定
+                agent.destination = target.position;
+            }
         }
     }
 
@@ -35,7 +39,7 @@ public class EnemyAI : MonoBehaviour
     {
         if (other.gameObject.name == "Player")
         {
-            Debug.Log("プレイヤーが範囲外へ");
+            anim.SetBool("Change", false);
         }
     }
 }
