7928d206da108687b6a025f7eb97a3c79301e3b6
Mokou2201207 <24051905@d.anabuki-college.net>
2026-05-13 14:24:40 +0900

    止まる、走る、歩くことで手振れ感を細かく変更

--- HorrorGames/Assets/Hamamoto/Aseets/Map/pixel horror abandoned rural  train station/scripts/PlayerMovement.cs
+++ HorrorGames/Assets/Hamamoto/Aseets/Map/pixel horror abandoned rural  train station/scripts/PlayerMovement.cs
@@ -1,10 +1,14 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Cinemachine;
 public class PlayerMovement : MonoBehaviour
 {
     [Header("プレイヤー"), SerializeField]
     private CharacterController controller;
+
+    [Header("シネマシーンのカメラをアタッチ"),SerializeField]
+    private CinemachineVirtualCamera virtualCamera;
 
     [Header("移動速度")]
     [SerializeField] private float walkSpeed = 5f;
@@ -15,6 +19,7 @@ public class PlayerMovement : MonoBehaviour
     [Header("ジャンプ力"), SerializeField]
     private float JumpHight = 1;
 
+    private CinemachineBasicMultiChannelPerlin noise;
     //現在のスピードを保管変数
     private float speed;
     Vector3 V;
@@ -22,6 +27,12 @@ public class PlayerMovement : MonoBehaviour
     {
         //格納
         controller = gameObject.GetComponent<CharacterController>();
+
+        if (virtualCamera != null)
+        {
+            // Noiseの設定部分を取得
+            noise = virtualCamera.GetCinemachineComponent<CinemachineBasicMultiChannelPerlin>();
+        }
     }
 
 
@@ -32,20 +43,41 @@ public class PlayerMovement : MonoBehaviour
             V.y = -2f;
         }
 
-        //shiftで移動速度変化
-        if (Input.GetKey(KeyCode.LeftShift))
-        {
-            speed=runSpeed;
-        }
-        else
-        {
-            speed = walkSpeed;
-        }
-
         float x = Input.GetAxis("Horizontal");
         float z = Input.GetAxis("Vertical");
 
         Vector3 move = transform.right * x + transform.forward * z;
+
+        if (move.magnitude > 0.1f)
+        {
+            //shiftで移動速度変化
+            if (Input.GetKey(KeyCode.LeftShift))
+            {
+                //画面の揺れも変化
+                //揺れの大きさ
+                noise.m_AmplitudeGain = 1.3f;
+                //揺れの速さ
+                noise.m_FrequencyGain = 0.3f;
+                speed = runSpeed;
+            }
+            else
+            {
+                //画面の揺れも変化
+                //揺れの大きさ
+                noise.m_AmplitudeGain = 0.7f;
+                //揺れの速さ
+                noise.m_FrequencyGain = 0.3f;
+                speed = walkSpeed;
+            }
+        }
+        else
+        {
+            noise.m_AmplitudeGain = 0.3f; 
+            noise.m_FrequencyGain = 0.1f;
+            speed = 0f;
+        }
+
+
 
         // 移動実行
         controller.Move(move * speed * Time.deltaTime);
