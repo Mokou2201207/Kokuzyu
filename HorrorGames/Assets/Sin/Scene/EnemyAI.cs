6fe51f8c719b8eca3fdaab9d4cad3f8380ecc003
Mokou2201207 <24051905@d.anabuki-college.net>
2026-05-13 11:19:15 +0900

    敵修正

--- HorrorGames/Assets/Sin/Scene/EnemyAI.cs
+++ HorrorGames/Assets/Sin/Scene/EnemyAI.cs
@@ -3,43 +3,50 @@ using UnityEngine.AI;
 
 public class EnemyAI : MonoBehaviour
 {
+    [Header("Playerをアタッチ")]
     public Transform target;
 
-    private NavMeshAgent agent;
-    private Animator anim;
-
+    [Header("コンポーネント（自動）")]
+    [SerializeField] private NavMeshAgent agent;
+    [SerializeField] private Animator anim;
     void Start()
     {
         agent = GetComponent<NavMeshAgent>();
-        anim = GetComponent<Animator>();
+        anim = GetComponentInChildren<Animator>();
     }
 
     void Update()
     {
-        
+        if (target != null)
+        {
+            //追跡処理
+            agent.destination = target.position;
+        }
     }
 
+    /// <summary>
+    /// トリガーに入ったら攻撃
+    /// </summary>
+    /// <param name="other"></param>
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.name == "Player")
         {
             anim.SetBool("Change", true);
-
-            transform.LookAt(target);
-
-            if (target != null)
-            {
-                // ターゲットの位置を目的地として設定
-                agent.destination = target.position;
-            }
         }
     }
 
+    /// <summary>
+    /// トリガーから外れたら攻撃停止
+    /// </summary>
+    /// <param name="other"></param>
     private void OnTriggerExit(Collider other)
     {
         if (other.gameObject.name == "Player")
         {
             anim.SetBool("Change", false);
+
+            agent.destination = transform.position;
         }
     }
 }
