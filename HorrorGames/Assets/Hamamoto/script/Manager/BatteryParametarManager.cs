eb764ef70402ba9b7bbe5ff160b2e321a2efcd3e
Mokou2201207 <24051905@d.anabuki-college.net>
2026-05-21 15:09:14 +0900

    懐中電灯のバッテリーが消費できるように可能※バッテリー切れでライト&霧が多くなる

--- HorrorGames/Assets/Hamamoto/script/Manager/BatteryParametarManager.cs
+++ HorrorGames/Assets/Hamamoto/script/Manager/BatteryParametarManager.cs
@@ -1,18 +1,68 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.UI;
+/// <summary>
+/// バッテリーの消費などの処理
+/// </summary>
 public class BatteryParametarManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [Header("バッテリースライダー"), SerializeField]
+    private Slider batterySlider;
+
+    [Header("バッテリが持つ時間"), SerializeField]
+    private float batteryConsumptionTime = 180f;
+
+    [Header("バッテリー切れ時の霧の濃さ")]
+    [SerializeField] private float deadBatteryFogDensity = 0.5f;
+
+    [Header("コンポーネント自動")]
+    [SerializeField] private Light flashlight;
+
+    //バッテリがそこを付いたかどうか
+    private bool isBatteryDead = false;
+
+    private void Start()
     {
-        
+        //格納
+        flashlight = GetComponent<Light>();
+
+        //ゲーム開始時にスライダーをMAXにしておく
+        if (batterySlider != null)
+        {
+            batterySlider.value = batterySlider.maxValue;
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        if (batterySlider == null) return;
+        //バッテリが底をついてなければ消費する
+        if (!isBatteryDead)
+        {
+            //一秒で減らすべきの量を特定
+            float decreasePerSecound = batterySlider.maxValue / batteryConsumptionTime;
+            //特定した秒数で減らしていく
+            batterySlider.value -= decreasePerSecound * Time.deltaTime;
+
+            //バッテリの底が付いたら
+            if (batterySlider.value <= 0f)
+            {
+                // マイナスにならないように0に固定
+                batterySlider.value = 0f;
+                isBatteryDead = true;
+
+                Debug.Log("バッテリーが切れました！");
+
+                //ライトを消す
+                if (flashlight != null)
+                {
+                    flashlight.enabled = false;
+                }
+
+                //霧を濃く
+                RenderSettings.fogDensity = deadBatteryFogDensity;
+            }
+        }
     }
 }
