dab11d27f5e700145769a88b3f0924bdc52bb3a7
Mokou2201207 <24051905@d.anabuki-college.net>
2026-05-22 11:13:11 +0900

    バッテリーを取得可能に

--- HorrorGames/Assets/Hamamoto/script/Manager/BatteryParametarManager.cs
+++ HorrorGames/Assets/Hamamoto/script/Manager/BatteryParametarManager.cs
@@ -16,8 +16,16 @@ public class BatteryParametarManager : MonoBehaviour
     [Header("バッテリー切れ時の霧の濃さ")]
     [SerializeField] private float deadBatteryFogDensity = 0.5f;
 
+    [Header("バッテリーを補充するSE")]
+    [SerializeField] private AudioClip supplementBatterySE;
+    [Header("充電が切れるSE")]
+    [SerializeField] private AudioClip deadBatterySE;
+    [Header("ゲージが赤くなるアニメーション")]
+    [SerializeField] private Animator animator;
+
     [Header("コンポーネント自動")]
     [SerializeField] private Light flashlight;
+    [SerializeField] private AudioSource audioSource;
 
     //バッテリがそこを付いたかどうか
     private bool isBatteryDead = false;
@@ -26,6 +34,7 @@ public class BatteryParametarManager : MonoBehaviour
     {
         //格納
         flashlight = GetComponent<Light>();
+        audioSource = GetComponent<AudioSource>();
 
         //ゲーム開始時にスライダーをMAXにしておく
         if (batterySlider != null)
@@ -45,9 +54,25 @@ public class BatteryParametarManager : MonoBehaviour
             //特定した秒数で減らしていく
             batterySlider.value -= decreasePerSecound * Time.deltaTime;
 
+            //ライトを付ける
+            if (flashlight != null)
+            {
+                flashlight.enabled = true;
+            }
+
+            //霧を濃く
+            RenderSettings.fogDensity =0.15f;
+
             //バッテリの底が付いたら
             if (batterySlider.value <= 0f)
             {
+                //二重防止
+                if (!isBatteryDead)
+                {
+                    //SE
+                    audioSource.PlayOneShot(deadBatterySE);
+                }
+
                 // マイナスにならないように0に固定
                 batterySlider.value = 0f;
                 isBatteryDead = true;
@@ -64,5 +89,30 @@ public class BatteryParametarManager : MonoBehaviour
                 RenderSettings.fogDensity = deadBatteryFogDensity;
             }
         }
+
+        //ゲージが0.2以下になったら赤色に
+        if (batterySlider.value<=0.2f)
+        {
+            animator.SetBool("MetarRed", true);
+        }
+        else
+        {
+            animator.SetBool("MetarRed", false);
+        }
+    }
+
+    /// <summary>
+    /// バッテリーを補充する処理
+    /// </summary>
+    public void SupplementBattery()
+    {
+        if (batterySlider == null) return;
+
+        //SE
+        audioSource.PlayOneShot(supplementBatterySE);
+
+        //バッテリーをマックスに
+        batterySlider.value = batterySlider.maxValue;
+        isBatteryDead = false;
     }
 }
