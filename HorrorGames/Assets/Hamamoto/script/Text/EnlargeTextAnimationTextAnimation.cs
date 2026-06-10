4b0c927d0de3d87f270836e49b3e9eda8c14891d
Mokou2201207 <24051905@d.anabuki-college.net>
2026-06-10 15:16:56 +0900

    タイトル追加（途中段階）

---
+++ HorrorGames/Assets/Hamamoto/script/Text/EnlargeTextAnimationTextAnimation.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using DG.Tweening;
+using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement;
+
+/// <summary>
+/// Textにcursorを合わせたら拡大する仕組み
+/// </summary>
+public class EnlargeTextAnimation : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+
+    [Header("アニメーション設定")]
+    [SerializeField] private float hoverScale = 1.15f; // どのくらい大きくするか
+    [SerializeField] private float duration = 0.2f;    // アニメーションの時間
+
+    private Vector3 defaultScale;
+    private Button button;
+
+    private void Start()
+    {
+        // ビルド時にFPSが上がりすぎてカメラが暴れるのを防ぐため、フレームレートを固定
+        Application.targetFrameRate = 60;
+
+
+        defaultScale = transform.localScale;
+
+        button = GetComponent<Button>();
+    }
+
+    /// <summary>
+    /// カーソルにTextが当たってる間拡大
+    /// </summary>
+    /// <param name="eventData"></param>
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        transform.DOScale(defaultScale * hoverScale, duration).SetEase(Ease.OutBack);
+    }
+
+
+    /// <summary>
+    /// カーソルからTextを外した場合縮小
+    /// </summary>
+    /// <param name="eventData"></param>
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        transform.DOScale(defaultScale, duration).SetEase(Ease.Linear);
+    }
+
+
+
+    /// <summary>
+    /// ゲーム終了
+    /// </summary>
+    public void OnQuitClick()
+    {
+        Debug.Log("ゲーム終了");
+        Application.Quit();
+    }
+
+
+}
---
+++ HorrorGames/Assets/Hamamoto/script/Title/LightFlicker.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightFlicker : MonoBehaviour
+{
+    private Light targetLight;
+
+    [Header("点滅の激しさ（小さいほどチラつく）")]
+    [SerializeField] private float flickerSpeed = 0.05f;
+
+    [Header("ライトの強さの範囲")]
+    [SerializeField] private float minIntensity = 0.2f;
+    [SerializeField] private float maxIntensity = 1.2f;
+
+    private void Start()
+    {
+        targetLight = GetComponent<Light>();
+        StartCoroutine(FlickerRoutine());
+    }
+
+    private System.Collections.IEnumerator FlickerRoutine()
+    {
+        while (true)
+        {
+            // ライトの有効・無効をランダムに切り替える
+            targetLight.enabled = Random.value > 0.1f; 
+
+            // 明るさもランダムに変える
+            targetLight.intensity = Random.Range(minIntensity, maxIntensity);
+
+            // 次の点滅までの待機時間も少しランダムにする
+            yield return new WaitForSeconds(Random.Range(0.01f, flickerSpeed));
+        }
+    }
+}
